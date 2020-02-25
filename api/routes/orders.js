16d834008d4da81176b7623b20e08bb07616c441
Suh Ryn <R@R.local>
2020-02-25 22:38:40 +0900

    shoppingmall

---
+++ api/routes/orders.js
@@ -0,0 +1,15 @@
+// express loading
+const express = require('express');
+// express안에 있는 모듈 중 Router를 쓸 것, 이것을 router라고 상수한다
+const router = express.Router();
+
+// orders와 관련된 api, order CRUD는 여기서 진행됨
+
+router.get('/', (req, res) => {
+    res.json({
+        msg: "orders data get"
+    });
+});
+
+// router를 모듈화 시켜서 내보낸다.
+module.export = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+// express를 불러온 것
+const express = require('express');
+"hello, my name is Ryn Suh"
+
+const app = express();
+
+
+
+// server.js 파일에서 라우트를 만든 후 상수화 시킴
+// require(경로)를 orderRoutes, productsRoute로 상수화 시킴
+const productsRoute = require('/api/routes/products');
+const ordersRoute = require('api/routes/orders');
+
+
+
+app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
+
+
+
+
+// 포트는 3000번
+const port = 3000;
+// app중의 .listen 이라는 모듈을 실행, console.log를 통해 서버가 실행되면 "server started.."라는 스트링을 보이게 한다.
+app.listen(port, console.log("server started.."));
